15d087df28b29fbbc2099bb6210421c17d027e28
HariShankarSrungavarapu <s.harishankar333@gmail.com>
2016-08-31 12:31:32 +0530

    Added Association between USer and Recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,6 +10,7 @@ class RecipesController < ApplicationController
 
   def create
   	@recipe = Recipe.new(recipe_params)
+    @recipe.user_id = current_user.id
 
     if @recipe.save
       redirect_to @recipe, notice: "Successfully created New Recipe"
@@ -42,7 +43,7 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params  
-    params.require(:recipe).permit(:title, :description, :image, ingredients_attributes: [:id, :name,:_destroy], directions_attributes: [:id, :step, :_destroy]) 
+    params.require(:recipe).permit(:title, :description, :user_id, :image, ingredients_attributes: [:id, :name,:_destroy], directions_attributes: [:id, :step, :_destroy]) 
   end
   
   def find_recipe
